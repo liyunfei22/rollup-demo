99288b2eee8f1e62ce77db607db09ec2da199f72
李云飞 <liyunfei02@daojia-inc.com>
2021-12-11 17:40:55 +0800

    feat: 调整

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import foo from './foo.js'
 import { version } from '../package.json'
 import answer from 'the-answer'
 import _ from 'lodash'
-// export default function() {
-//   console.log('version' + version)
-// }
+console.log(version)
 export default () => {
   console.log(`the answer is ${answer}`);
 }
